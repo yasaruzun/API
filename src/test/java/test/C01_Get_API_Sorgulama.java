36c05b5f7c64581e9a14485c8775b131552ad305
Yasar UZUN <116967168+yasaruzun@users.noreply.github.com>
2023-01-28 21:58:12 +0300

    first commit

---
+++ src/test/java/test/C01_Get_API_Sorgulama.java
@@ -0,0 +1,15 @@
+package test;
+
+public class C01_Get_API_Sorgulama {
+
+    //
+    //        https://restful-booker.herokuapp.com/booking/256884 url’ine
+    //        bir GET request gonderdigimizde donen Response’un,
+    //        status code’unun 200,
+    //        ve content type’inin application/json; charset=utf-8,
+    //        ve Server isimli Header’in degerinin Cowboy,
+    //        ve status Line’in HTTP/1.1 200 OK
+    //        ve response suresinin 5 sn’den kisa oldugunu manuel olarak test ediniz.
+
+
+}
